b68a783235db98014562c20ed0fc77d87a4186a8
gregford117 <gregford117@live.com>
2017-01-23 22:46:20 +0000

    More logic progress (not much) line 16 stil buggy thought (aim to fix in spare time tommorow)

--- GtinValidator.py
+++ GtinValidator.py
@@ -11,11 +11,16 @@ def Gtin(digitsToCalculate):
 	input_len = len(str(digitsToCalculate))
 
 	if ValidInput(input_len):
+		loop_num = 0 
 		for char in list(str(digitsToCalculate)):							
-			
+			if loop_num % 2 == ( input_len % 2 ):
+				print(char)			
+			#else:
+			#	print('loop num = %s\ncurrent char = %s' % (loop_num, char))				
+
+			loop_num += 1
 	else:
 		raise ValueError('Incorrect Input Length')
 
-if __name__ == '__main__':
-	#Gtin(1212121)
-	Gtin()
+if __name__ == '__main__':	
+	Gtin(1234567)
