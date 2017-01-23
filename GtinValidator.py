049819b25fca92aeb32db00fa4f9b3c2cd7b47e8
gregford117 <gregford117@live.com>
2017-01-23 21:17:19 +0000

    Added more basic logic

--- GtinValidator.py
+++ GtinValidator.py
@@ -1,1 +1,20 @@
 from math import ceil
+
+def RoundUp(x):
+	return ceil(x / 10.0) * 10
+
+def ValidInput(input_length):
+	allowed_lengths = [7, 11, 12, 13, 16, 17]
+	return input_length in allowed_lengths
+
+def Gtin(digitsToCalculate):
+	input_len = len(str(digitsToCalculate))
+
+	if ValidInput(input_len):
+		for char in list(str(digitsToCalculate)):							
+			
+	else:
+		raise ValueError('Incorrect Input Length')
+
+if __name__ == '__main__':
+	Gtin(1212121)
